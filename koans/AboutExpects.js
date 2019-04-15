aa88dc236f9dd3795dc0f3c7b396bc84ab43e240
Ken Huh <ken123777@gmail.com>
2019-04-15 16:09:32 +0900

    Update latest

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -3,15 +3,18 @@ describe('About Expects', function() {
   // We shall contemplate truth by testing reality, via spec expectations.
   it('should expect true', function() {
 
-    // Your journey begins here: Replace the word false with true 
+    // Your journey begins here: Replace the word false with true
     expect(false).toBeTruthy();
   });
 
   // To understand reality, we must compare our expectations against reality.
   it('should expect equality', function() {
+    // TODO: Replace FILL_ME_IN with your solution.
     var expectedValue = FILL_ME_IN;
     var actualValue = 1 + 1;
 
+    // expect 함수에 넘겨지는 `actualValue === expectedValue` 부분에 대해서만 생각하시면 됩니다.
+    // expect, toBeTruthy 등은 어떤 주어진 값을 판별하는 용도의 Jasmine Library에서 제공하는 함수입니다.
     expect(actualValue === expectedValue).toBeTruthy();
   });
 
